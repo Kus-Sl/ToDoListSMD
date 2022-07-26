c824fedf64dafcb55b9a2a0cd989dc1459b8b247
Kusakin_Sl <kusakin.slava@inbox.ru>
2022-07-26 15:18:28 +0300

    Initial Commit

---
+++ ToDoListSMD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListSMD
+//
+//  Created by Вячеслав Кусакин on 26.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
